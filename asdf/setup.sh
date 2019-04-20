6a97bca35c1284c2e5cb26f779e4cca92c88559f
grimoh <dev@grimoh.net>
2019-04-20 23:05:31 +0900

    Fix asdf setup

--- asdf/setup.sh
+++ asdf/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-goversion={1:-"1.12.1"}
+goversion=${1:-"1.12.1"}
 cp -p tool-versions ~/.tool-versions
 echo -e "# asdf setting\n. $HOME/.asdf/asdf.sh\n. $HOME/.asdf/completions/asdf.bash\n" >> ~/.bashrc
 echo -e "# golang\nexport GO111MODULE=on\nexport GOROOT=$HOME/.asdf/installs/golang/<GOVERSION>/go\nexport GOPATH=$HOME/.go\nexport PATH=$GOPATH/bin:$PATH\n" >> ~/.bashrc
