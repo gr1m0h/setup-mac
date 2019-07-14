ef1d66331473b7f07c94e5b18c242e5b2b33cc9e
grimoh <dev@grimoh.net>
2019-07-14 15:56:47 +0900

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,11 @@
 set -x fish_greeting
 
 # alias
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
 alias vi="nvim"
-alias cat="ccat -G Keyword="purple" -G Decimal="blue" -G Plaintext="blink""
 alias k="kubectl"
+alias cat="ccat -G Keyword="purple" -G Decimal="blue" -G Plaintext="blink""
+alias c="clear"
