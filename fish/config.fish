b7f0b5c61c638abf43bf5fd9c4716063d1887f97
grimoh <dev@grimoh.net>
2020-07-24 18:55:23 +0900

    Update fish/

--- fish/config.fish
+++ fish/config.fish
@@ -11,12 +11,9 @@ alias ....="cd ../../.."
 
 alias vi="nvim"
 alias k="kubectl"
-alias kc="kubectx"
-alias kn="kubens"
 alias c="clear"
 
 # coc
 alias cocls="npm --prefix=$HOME/.config/coc/extensions/ list --depth=0"
 alias cocin="npm --prefix=$HOME/.config/coc/extensions/ install"
 alias cocrm="npm --prefix=$HOME/.config/coc/extensions/ remove"
-
