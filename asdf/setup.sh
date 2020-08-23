628b20fba5023ef608d33d00d967986d6082e4a8
grimoh <dev@grimoh.net>
2020-08-23 18:39:24 +0900

    Update asdf config

--- asdf/setup.sh
+++ asdf/setup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 goversion=${1:-"1.14.6"}
 cp -p tool-versions ~/.tool-versions
-echo -e "# asdf setting\n. $HOME/.asdf/asdf.sh\n. $HOME/.asdf/completions/asdf.bash\n" >> ~/.bashrc
-echo -e "# golang\nexport GO111MODULE=on\nexport GOROOT=$HOME/.asdf/installs/golang/<GOVERSION>/go\nexport GOPATH=$HOME/.go\nexport PATH=$GOPATH/bin:$PATH\n" >> ~/.bashrc
-echo -e "# kubectl\nexport KUBECONFIG=$HOME/.kube/config/config.yaml\n" >> ~/.bashrc
-grep -l "<GOVERSION>" ~/.bashrc | xargs sed -i.bak -e "s/<GOVERSION>/$goversion/g"
+echo -e "# asdf-vm\nsource ~/.asdf/asdf.fish\nmkdir -p ~/.config/fish/completions; and cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions\n" >> ~/.bashrc
